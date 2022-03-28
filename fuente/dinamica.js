cd2cc8eb97977e4fd5b6d7128b05ac38ee1b3f50
Aaron Alicastro <aalicastro5@gmail.com>
2022-03-28 09:26:08 -0500

    Creado la interfaz principal

--- fuente/dinamica.js
+++ fuente/dinamica.js
@@ -1,6 +1,16 @@
 const letrasAimation = document.querySelectorAll(".letrasAimation");
 const tituloBienbenida = document.querySelector("#tituloBienbenida");
 const body = document.querySelector("#body");
+const containerIndex = document.querySelector("#containerIndex");
+const footer = document.querySelector("#footer");
+///////////////////////// constantes de la interactividad //////////////////////////////////
+const masMe = document.querySelector("#masMe");
+const menu = document.querySelector("#menu"); var abrioMenu = false;
+const containerOpcionesAboutMe = document.querySelector("#containerOpcionesAboutMe");
+const descripcionOpcioneMe = document.querySelectorAll(".descripcionOpcioneMe");
+const hobbies = document.querySelectorAll(".hobbies");
+const containerCartillaHobies = document.querySelector("#containerCartillaHobies");
+const cerrarCartilla = document.querySelector("#cerrarCartilla");
 
 window.addEventListener("load", (e)=>{
 	setTimeout((e)=>{ 
@@ -42,5 +52,114 @@ window.addEventListener("load", (e)=>{
 		letrasAimation[9].style.animation = "caidaPalabras9 5s forwards";
 		body.style.animation = "aparecerColorFondo 5s forwards";
 		setTimeout((e)=>{ tituloBienbenida.style.display = "none"}, 5001);
+		setTimeout((e)=>{ containerIndex.style.display = "flex";
+						containerIndex.style.flexDirection = "row";
+						containerIndex.style.justifyContent = "space-around";
+						containerIndex.style.height = "93vh";
+						containerIndex.style.overflowY = "auto";
+						footer.style.display = "flex";
+						footer.style.flexDirection = "row";
+						footer.style.justifyContent = "center";
+						footer.style.padding = "10px";
+		}, 5002);
 	},3000);
+});
+//////////////////////////// funciones /////////////////////////////////////
+menu.addEventListener("click", (e)=>{
+	if (!abrioMenu) {
+		abrioMenu = true;
+		menu.style.animation = "aperturaAboutMeLetraGrande 2s forwards";
+		masMe.style.animation = "aperturaAboutMe 2s forwards";
+		setTimeout((e)=>{ containerOpcionesAboutMe.style.display = "block";}, 2001);
+	}else{
+		abrioMenu = false;
+		containerOpcionesAboutMe.style.display = "none";
+		menu.style.animation = "cierreAboutMeLetraGrande 2s forwards";
+		masMe.style.animation = "cierreAboutMe 2s forwards";
+		descripcionOpcioneMe[0].innerHTML = `Si tienes interés en mi trabajo, tal vez quieras consultar mi hoja de vida; a solo un click.`;
+		descripcionOpcioneMe[1].innerHTML = `Si así lo deseas, puedes ver los proyectos que he hecho, desde los más recientes a los menos recientes.`;
+		descripcionOpcioneMe[2].innerHTML = `¿Quieres ver mi perfil en linkedln?, a solo un click.`;
+		descripcionOpcioneMe[3].innerHTML = `A solo un click.`;
+	}
+});
+descripcionOpcioneMe[0].addEventListener("click", (e)=>{
+	descripcionOpcioneMe[0].innerHTML += `<br><br> <b>Ups, sitio equivocado</b>`;
+});
+descripcionOpcioneMe[1].addEventListener("click", (e)=>{
+	descripcionOpcioneMe[1].innerHTML += `<br><br> <b>Ups, sitio equivocado</b>`;
+});
+descripcionOpcioneMe[2].addEventListener("click", (e)=>{
+	descripcionOpcioneMe[2].innerHTML += `<br><br> <b>Ups, sitio equivocado</b>`;
+});
+descripcionOpcioneMe[3].addEventListener("click", (e)=>{
+	descripcionOpcioneMe[3].innerHTML += `<br><br> <b>Ups, sitio equivocado</b>`;
+});
+cerrarCartilla.addEventListener("click", (e)=>{
+	containerCartillaHobies.style.display = "none";
+});
+hobbies[0].addEventListener("click", (e)=>{
+	containerCartillaHobies.style.display = "block";
+	containerCartillaHobies.style.position = "absolute";
+	containerCartillaHobies.style.top = "20px";
+	containerCartillaHobies.style.left = "60vh";
+	containerCartillaHobies.style.padding = "15px";
+	containerCartillaHobies.style.width = "300px";
+	containerCartillaHobies.style.height = "300px";
+	containerCartillaHobies.style.background = "#005aa8";
+	containerCartillaHobies.style.borderRadius = "12px";
+});
+hobbies[1].addEventListener("click", (e)=>{
+	containerCartillaHobies.style.display = "block";
+	containerCartillaHobies.style.position = "absolute";
+	containerCartillaHobies.style.top = "20px";
+	containerCartillaHobies.style.left = "60vh";
+	containerCartillaHobies.style.padding = "15px";
+	containerCartillaHobies.style.width = "300px";
+	containerCartillaHobies.style.height = "300px";
+	containerCartillaHobies.style.background = "#005aa8";
+	containerCartillaHobies.style.borderRadius = "12px";
+});
+hobbies[2].addEventListener("click", (e)=>{
+	containerCartillaHobies.style.display = "block";
+	containerCartillaHobies.style.position = "absolute";
+	containerCartillaHobies.style.top = "20px";
+	containerCartillaHobies.style.left = "60vh";
+	containerCartillaHobies.style.padding = "15px";
+	containerCartillaHobies.style.width = "300px";
+	containerCartillaHobies.style.height = "300px";
+	containerCartillaHobies.style.background = "#005aa8";
+	containerCartillaHobies.style.borderRadius = "12px";
+});
+hobbies[3].addEventListener("click", (e)=>{
+	containerCartillaHobies.style.display = "block";
+	containerCartillaHobies.style.position = "absolute";
+	containerCartillaHobies.style.top = "20px";
+	containerCartillaHobies.style.left = "60vh";
+	containerCartillaHobies.style.padding = "15px";
+	containerCartillaHobies.style.width = "300px";
+	containerCartillaHobies.style.height = "300px";
+	containerCartillaHobies.style.background = "#005aa8";
+	containerCartillaHobies.style.borderRadius = "12px";
+});
+hobbies[4].addEventListener("click", (e)=>{
+	containerCartillaHobies.style.display = "block";
+	containerCartillaHobies.style.position = "absolute";
+	containerCartillaHobies.style.top = "20px";
+	containerCartillaHobies.style.left = "60vh";
+	containerCartillaHobies.style.padding = "15px";
+	containerCartillaHobies.style.width = "300px";
+	containerCartillaHobies.style.height = "300px";
+	containerCartillaHobies.style.background = "#005aa8";
+	containerCartillaHobies.style.borderRadius = "12px";
+});
+hobbies[5].addEventListener("click", (e)=>{
+	containerCartillaHobies.style.display = "block";
+	containerCartillaHobies.style.position = "absolute";
+	containerCartillaHobies.style.top = "20px";
+	containerCartillaHobies.style.left = "60vh";
+	containerCartillaHobies.style.padding = "15px";
+	containerCartillaHobies.style.width = "300px";
+	containerCartillaHobies.style.height = "300px";
+	containerCartillaHobies.style.background = "#005aa8";
+	containerCartillaHobies.style.borderRadius = "12px";
 });
